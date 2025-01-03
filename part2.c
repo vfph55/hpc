3f883623b7ef7ca30e7a381bef02b943a652afb0
xiaoqian CHENG <vfph55@durham.ac.uk>
2025-01-03 00:12:37 +0000

    d

--- part2.c
+++ part2.c
@@ -4,16 +4,10 @@
 #include <mpi.h>                // MPI header
 
 void init(double u[N][N], double v[N][N]) {
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    int rows_per_process = N / size; // Determine local rows per process
-    for (int i = 0; i < rows_per_process; i++) {
+    for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            int global_i = rank * rows_per_process + i; // Global index for rows
-            u[i][j] = ulo + (uhi - ulo) * 0.5 * (1.0 + tanh((global_i - N / 2) / 16.0));
-            v[i][j] = vlo + (vhi - vlo) * 0.5 * (1.0 + tanh((j - N / 2) / 16.0));
+            u[i][j] = ulo + (uhi-ulo)*0.5*(1.0 + tanh((i-N/2)/16.0));
+            v[i][j] = vlo + (vhi-vlo)*0.5*(1.0 + tanh((j-N/2)/16.0));
         }
     }
 }
@@ -24,129 +18,140 @@ void dxdt(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int rows_per_process = N / size;
+    int start_row = rank * rows_per_process;
+    int end_row = (rank == size - 1) ? N : start_row + rows_per_process;
+
     double lapu, lapv;
+    int up, down, left, right;
 
-    // Boundary exchange to handle array boundaries
-    if (rank != 0) {
-        MPI_Send(u[0], N, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD);
-    }
-    if (rank != size - 1) {
-        MPI_Send(u[rows_per_process - 1], N, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD);
-    }
-    
-    if (rank != 0) {
-        MPI_Recv(u[-1], N, MPI_DOUBLE, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    if (rank != size - 1) {
-        MPI_Recv(u[rows_per_process], N, MPI_DOUBLE, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
+    // Exchange ghost rows
+    if (rank > 0)
+        MPI_Send(u[start_row], N, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
+    if (rank < size - 1)
+        MPI_Send(u[end_row-1], N, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD);
+    if (rank < size - 1)
+        MPI_Recv(u[end_row], N, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if (rank > 0)
+        MPI_Recv(u[start_row-1], N, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    for (int i = 0; i < rows_per_process; i++) {
+    // Same for v
+    if (rank > 0)
+        MPI_Send(v[start_row], N, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD);
+    if (rank < size - 1)
+        MPI_Send(v[end_row-1], N, MPI_DOUBLE, rank+1, 1, MPI_COMM_WORLD);
+    if (rank < size - 1)
+        MPI_Recv(v[end_row], N, MPI_DOUBLE, rank+1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    if (rank > 0)
+        MPI_Recv(v[start_row-1], N, MPI_DOUBLE, rank-1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    for (int i = start_row; i < end_row; i++) {
         for (int j = 0; j < N; j++) {
-            int global_i = rank * rows_per_process + i;
+            down = (i == 0) ? i : i-1;
+            up = (i == N-1) ? i : i+1;
+            left = (j == 0) ? j : j-1;
+            right = (j == N-1) ? j : j+1;
 
-            // Determine neighboring indices, handling boundaries accordingly
-            int up = (i == rows_per_process - 1) ? i : i + 1;      // Local up
-            int down = (i == 0) ? 0 : i - 1;                       // Local down
-            int left = (j == 0) ? 0 : j - 1;                       // Local left
-            int right = (j == N - 1) ? N - 1 : j + 1;             // Local right
-
-            lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] - 4.0 * u[i][j];
-            lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] - 4.0 * v[i][j];
-            du[i][j] = DD * lapu + f(u[i][j], v[i][j]) + R * stim(global_i, j);
-            dv[i][j] = d * DD * lapv + g(u[i][j], v[i][j]);
+            lapu = u[up][j] + u[down][j] + u[i][left] + u[i][right] - 4.0*u[i][j];
+            lapv = v[up][j] + v[down][j] + v[i][left] + v[i][right] - 4.0*v[i][j];
+            du[i][j] = DD*lapu + f(u[i][j], v[i][j]) + R*stim(i,j);
+            dv[i][j] = d*DD*lapv + g(u[i][j], v[i][j]);
         }
     }
 }
 
 void step(double du[N][N], double dv[N][N], double u[N][N], double v[N][N]) {
-    int rows_per_process, i, j;
-    MPI_Comm_size(MPI_COMM_WORLD, &rows_per_process);
-    
-    for (i = 0; i < N / rows_per_process; i++) {
-        for (j = 0; j < N; j++) {
-            u[i][j] += dt * du[i][j];
-            v[i][j] += dt * dv[i][j];
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int rows_per_process = N / size;
+    int start_row = rank * rows_per_process;
+    int end_row = (rank == size - 1) ? N : start_row + rows_per_process;
+
+    for (int i = start_row; i < end_row; i++) {
+        for (int j = 0; j < N; j++) {
+            u[i][j] += dt*du[i][j];
+            v[i][j] += dt*dv[i][j];
         }
     }
 }
 
 double norm(double x[N][N]) {
-    double local_nrm = 0.0;
-    int rows_per_process;
-    MPI_Comm_size(MPI_COMM_WORLD, &rows_per_process);
-    for (int i = 0; i < N / rows_per_process; i++) {
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int rows_per_process = N / size;
+    int start_row = rank * rows_per_process;
+    int end_row = (rank == size - 1) ? N : start_row + rows_per_process;
+
+    double local_nrmx = 0.0;
+    for (int i = start_row; i < end_row; i++) {
         for (int j = 0; j < N; j++) {
-            local_nrm += x[i][j] * x[i][j];
+            local_nrmx += x[i][j]*x[i][j];
         }
     }
 
-    // Use MPI_Reduce to sum up the norms from all processes
-    double global_nrm = 0.0;
-    MPI_Reduce(&local_nrm, &global_nrm, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    return global_nrm;
+    double global_nrmx;
+    MPI_Allreduce(&local_nrmx, &global_nrmx, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    return global_nrmx;
 }
 
 int main(int argc, char** argv) {
-    // Start timing
-    double start_time = MPI_Wtime();
-
-    MPI_Init(&argc, &argv); // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    double start_time, end_time;
+    start_time = MPI_Wtime();
+
     double t = 0.0, nrmu, nrmv;
     double u[N][N], v[N][N], du[N][N], dv[N][N];
 
-    // Open the output file
-    FILE *fptr = fopen("part2.dat", "a"); // Append mode
+    FILE *fptr = NULL;
     if (rank == 0) {
+        fptr = fopen("part2.dat", "w");
         fprintf(fptr, "#t\t\tnrmu\t\tnrmv\n");
     }
 
-    // Initialize the state
+    // initialize the state
     init(u, v);
-    
-    // Time-loop
+
+    // time-loop
     for (int k = 0; k < M; k++) {
-        // Track the time
-        t = dt * k;
-
-        // Evaluate the PDE
+        // track the time
+        t = dt*k;
+        // evaluate the PDE
         dxdt(du, dv, u, v);
-
-        // Update the state variables u and v
+        // update the state variables u,v
         step(du, dv, u, v);
-        
-        // Calculate norms at given intervals, only on rank 0
-        if (k % m == 0) {
-            nrmu = norm(u); // Local norm
-            nrmv = norm(v); // Local norm
-            
-            // Communicate the norms to rank 0
-            double global_nrmu, global_nrmv;
-            MPI_Reduce(&nrmu, &global_nrmu, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-            MPI_Reduce(&nrmv, &global_nrmv, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-            // Print to console and write to file only from rank 0
+        if (k%m == 0) {
+            // calculate the norms
+            nrmu = norm(u);
+            nrmv = norm(v);
             if (rank == 0) {
-                printf("t = %2.1f\tu-norm = %2.5f\tv-norm = %2.5f\n", t, global_nrmu, global_nrmv);
-                fprintf(fptr, "%f\t%f\t%f\n", t, global_nrmu, global_nrmv);
+                printf("t = %2.1f\tu-norm = %2.5f\tv-norm = %2.5f\n", t, nrmu, nrmv);
+                fprintf(fptr, "%f\t%f\t%f\n", t, nrmu, nrmv);
             }
         }
     }
- 
-    // Stop timing
-    double end_time = MPI_Wtime();
-    double total_time = end_time - start_time;
 
     if (rank == 0) {
-        printf("Total time: %f seconds\n", total_time);
+        fclose(fptr);
     }
 
-    fclose(fptr);
-    MPI_Finalize(); // Finalize the MPI environment
+    end_time = MPI_Wtime();
+    double total_time = end_time - start_time;
+    double max_total_time;
+    MPI_Reduce(&total_time, &max_total_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        printf("Total execution time: %f seconds\n", max_total_time);
+    }
+
+    MPI_Finalize();
     return 0;
 }
